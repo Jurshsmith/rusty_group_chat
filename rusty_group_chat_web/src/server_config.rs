3e43db333b387835b78e62311e81d096ed32e40d
Joshua Oladele <jurshsmith@gmail.com>
2023-03-10 02:02:50 +0100

    Separate server config

---
+++ rusty_group_chat_web/src/server_config.rs
@@ -0,0 +1,33 @@
+use dotenvy::dotenv;
+
+#[derive(Debug)]
+pub struct ServerConfig {
+    pub host: String,
+    pub port: u16,
+}
+
+impl ServerConfig {
+    pub fn get() -> Self {
+        dotenv().ok();
+
+        let server_host = std::env::var("SERVER_HOST").unwrap_or(Self::default_host());
+        let server_port = std::env::var("SERVER_PORT").unwrap_or(Self::default_port());
+
+        ServerConfig {
+            host: server_host,
+            port: server_port.parse().unwrap(),
+        }
+    }
+
+    pub fn host_with_port(&self) -> String {
+        format!("{}{}", self.host, self.host)
+    }
+
+    fn default_host() -> String {
+        "127.0.0.1".to_string()
+    }
+
+    fn default_port() -> String {
+        "4003".to_string()
+    }
+}
