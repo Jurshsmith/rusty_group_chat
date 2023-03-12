418e9601ccacdfaeb1fa40d2530e941b8a604ca7
Joshua Oladele <jurshsmith@gmail.com>
2023-03-12 12:50:01 +0100

    Add user details to the WS connection

--- rusty_group_chat/src/user.rs
+++ rusty_group_chat/src/user.rs
@@ -5,21 +5,3 @@ pub struct User {
     pub name: String,
     // TODO: Add random color that can be used in CLI display
 }
-
-impl From<String> for User {
-    fn from(user: String) -> Self {
-        serde_json::from_str(&user).unwrap()
-    }
-}
-
-impl User {
-    pub fn empty() -> Self {
-        User {
-            name: "".to_string(),
-        }
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.name.is_empty()
-    }
-}
