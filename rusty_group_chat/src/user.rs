593995921f7ea5dcb9a0a98278caa79c77854137
Joshua Oladele <jurshsmith@gmail.com>
2023-03-12 21:22:37 +0100

    Expose from_name/1 and .is_equal_to/1 in User

--- rusty_group_chat/src/user.rs
+++ rusty_group_chat/src/user.rs
@@ -1,7 +1,17 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct User {
     pub name: String,
     // TODO: Add random color that can be used in CLI display
+}
+
+impl User {
+    pub fn from_name(name: String) -> User {
+        User { name }
+    }
+
+    pub fn is_equal_to(&self, user: &User) -> bool {
+        self.name == user.name
+    }
 }
